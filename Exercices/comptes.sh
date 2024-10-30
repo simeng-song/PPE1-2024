1786691760e1039b8d09e6f9dfb16099e8bc5b42
simeng-song <song_simeng2024@163.com>
2024-10-30 23:39:52 +0100

    Ajout du script comptes.sh

--- Exercices/comptes.sh
+++ Exercices/comptes.sh
@@ -4,9 +4,3 @@ for year in {2016..2018}; do
     comptes=$(grep -c 'Location' ~/PPE1/archive-7/$year/*.ann | wc -l)
     echo "Annotations en $year : $comptes"
 done
-
-git add ~/PPE1/PPE1-2024/Exercices/comptes.sh
-git commit -m "Ajout du script comptes.sh"
-git push origin main
-
-

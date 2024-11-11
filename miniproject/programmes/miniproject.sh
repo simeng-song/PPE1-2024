389056c5221c6f172c29dfa31da1f30255b8e795
simeng-song <song_simeng2024@163.com>
2024-11-11 23:41:50 +0100

    Modification du script.sh

--- miniproject/programmes/miniproject.sh
+++ miniproject/programmes/miniproject.sh
@@ -1,6 +1,26 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
-while read -r line
-do
-	echo $line
-done < "urls/fr.txt"
+if [ -z "$1" ]; then
+    echo "Erreur : Veuillez spécifier un fichier en argument."
+    exit 1
+fi
+
+echo -e "Numéro de ligne\tURL\tCode HTTP\tNombre de mots\tEncodage"
+
+LineNumber=1
+
+while read -r url; do
+	code_http=$(curl -o /dev/null -s -w "%{http_code}" "$url")
+	content=$(curl -s "$url")
+	encoding=$(echo "$content" | file -i - | awk -F "=" '{print $2}')
+	
+	if [ -z "$encoding" ]; then
+    encoding="Indéfini"  
+	fi
+
+	word_count=$(echo "$content" | wc -w)
+	
+	echo -e "${LineNumber}\t${url}\t${code_http}\t${encoding}\t${word_count}"	
+	((LineNumber++))
+	
+done < "$1"
